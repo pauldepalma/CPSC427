f98cfcc29376678465af4145e8ad82daf5b64ad8
Paul De Palma <depalma@gonzaga.edu>
2019-01-23 11:51:17 -0800

    fix knn2

--- knn2/kNN-Tst.py
+++ knn2/kNN-Tst.py
@@ -1,47 +1,24 @@
+from numpy import *
+from kNN import *
 
-from operator import itemgetter
 
-def calcDistances(unknown,movie_data):
-    dist_lst = []
-    for point in movie_data:
-      xdist = (point[0] - unknown[0])**2
-      ydist = (point[1] - unknown[1])**2
-      dist = (xdist + ydist) ** .05
-      item = (dist,point[2])
-      dist_lst.append(item)
-    return sorted(dist_lst,key=itemgetter(0))
-
-def count_votes(distances,k):
-    vote_count = {}
-    vote_count['r'] = 0
-    vote_count['a'] = 0
-    for i in range(k):
-        label = distances[i][1]
-        if vote_count[label] > 0:
-            vote_count[label] = vote_count[label] + 1
-        else:
-            vote_count[label] = 1
-
-    if (vote_count['r'] > vote_count['a']):
-        return 'r'
-    else:
-        return 'a'
+def make_data():
+    #group is matrix of size 6 rows X 2 columns
+    group = array([[3,104],[2,100],[1,81],[101,10],[99,5],[98,2]])
+    labels = ['r','r','r','a','a','a']
+    categories = {'r':'romance','a':'adventure'}
+    return group, labels, categories
+    
     
 def main():
-    unknown = (18,90)
+    #num kicks 1st parameter
+    #num kisses 2nd parameter
+    unknown = (90,18)  #90 kicks anbd 18 kisses. what kind of movie
     title = "Blade Runner"
-    k = 3
-    movie_data = [(3,104,'r'),(2,100,'r'),(1,81,'r'),(101,10,'a'),(99,5,'a'),
-                  (98,2,'a')]
+    k = 3 #knn parameter
     
-
-    distances = calcDistances(unknown, movie_data)
-    winner = count_votes(distances,k)
-    if winner == 'r':
-        type = 'romance'
-    else:
-        type = 'adventure'
-
-    print (title + ':' + type)
+    group, labels, categories= make_data()
+    type = classify(unknown,group,labels,k)
+    print (title + ':' + categories[type])
     
 main()
--- knn2/kNN.py
+++ knn2/kNN.py
@@ -1,44 +1,49 @@
 from numpy import *
 import operator
 
+'''
+in_pt is an array: [0,0] for example.  Calculate the distance from all points
+in the data to in_pt
+group is the matrix created in the calling program
+labels is the label vector from the calling program
+k is the nearest neighbor parameter
+returns the label with the greates number of votes
+'''
+def classify(in_pt, group, labels, k):
 
-#inPt is an array: [0,0] for example.  It is the point, the distance to we want
-#to calculate
-#group is the matrix from createDataSet
-#labels is the label vector above
-#k is the nearest neighbor parameter
-def classify0(inPt, group, labels, k):
-    distances = calcDistances(inPt,group)
+    #vector with ith entry containing euclidean distance from ith point in group
+    #to in_point
+    distances = calc_distances(in_pt,group)
 
-    #produce a list of indices to items in the distances vector.  index 0
-    #refers to the smallest item in distances, index 1 to the next smallest,
-    #and so forth. 
-    distancesIdx = distances.argsort()
+    #produce a list of indices to items in the distances vector.  index at
+    #position 0 is the index to the smallest item in distances, index 1 to
+    #the next smallest, and so forth
+    distances_idx = distances.argsort()
 
-    #determine how many times each label appears in the top k entries
-    voteCount = {}
+    #determine how many times each label appears in the smallest k entries
+    class_count = {}
     for i in range(k):
-        labelI = labels[distancesIdx[i]]
-        if labelI in voteCount:
-            voteCount[labelI] = voteCount[labelI] + 1
-        else:
-            voteCount[labelI] = 1
+        #extract index associated with ith entry in distances_idx
+        #use it to find the associated label
+        vote_label = labels[distances_idx[i]]
+        #add 1 to the label entry if > 0.  if it does not exist add 1 to 0
+        class_count[vote_label] = class_count.get(vote_label,0) + 1
 
-    #turn the dictionary into a list of tuples, sorting the tuples by the
-    #second item, i.e., the number of votes the label (the first item) got
-    #itemgetter gets the second (1) item in the dictionary.
-    #The idea here is that we're sorting the dictionary by the second item,
-    #the number of votes.
-    sortedVoteCount = sorted(voteCount.iteritems(), key=operator.itemgetter(1),
+    #class_count is a collection of label:vote pairs, where 'vote' is the number
+    #of votes gotten by the label.  sorted_vote_count is list of label,vote tuples, sorted by
+    #votes in reverse numerical order.  So, the 0th item in the list is the tuple containing the label
+    #with the most votes, the 1st item is the tuple containing the second highest vote count
+    #and so forth
+    sorted_vote_count = sorted(class_count.iteritems(), key=operator.itemgetter(1),
                              reverse = True)
-    #the first entry in sortedVoteCount is the label with the most votes
-    return sortedVoteCount[0][0] #0th item in 0th tuple
+    return sorted_vote_count[0][0] #0th item in 0th tuple, i.e, the label with the most votes
         
-
-   
-'''calculate the distance for each point in the original group to the new
-   point by Euclidean distance'''
-def calcDistances(inPt,group):
+'''
+Calculate the euclidean distance from in_pt to every point in the group matrix
+Returns a vector of distances to in_pt, in the same order as the points themselves
+appear in group
+'''
+def calc_distances(inPt,group):
     #get number of rows in group
     dataSetSize = group.shape[0]
     
