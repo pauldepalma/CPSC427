9086f3c870609ff8011caec83f929d12d84f38eb
Paul De Palma <depalma@gonzaga.edu>
2019-02-18 13:10:58 -0800

    Iterative Linear Regression

--- E-Linear-Regression/linear-regression1.py
+++ E-Linear-Regression/linear-regression1.py
@@ -1,12 +1,13 @@
 '''
 Iterative version of gradient descent for linear regression
-Code is adapted from and data comes from: ...github.com/mattnedrich/GradientDescentExample
+Code is adapted from and data comes from
+: ...github.com/mattnedrich/GradientDescentExample
 '''
-
-from numpy import *
+import numpy
+import csv
 import matplotlib.pyplot as plt
 
-def step_gradient(b_current, m_current, points, learningRate):
+def gradient(b_current, m_current, points, learningRate):
     b_gradient = 0
     m_gradient = 0
     N = float(len(points))
@@ -19,47 +20,38 @@ def step_gradient(b_current, m_current, points, learningRate):
     new_m = m_current - (learningRate * m_gradient)
     return [new_b, new_m]
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+def gradient_descent(points, starting_b, starting_m, learning_rate, num_iter):
     b = starting_b
     m = starting_m
-    for i in range(num_iterations):
-        b, m = step_gradient(b, m, array(points), learning_rate)
+    for i in range(num_iter):
+        b, m = gradient(b, m, points, learning_rate)
     return [b, m]
-
-def compute_mean_error(points,b,m):
-    error = 0
-    for i in range(0, len(points)):
-        x = points[i,0]
-        y = points[i,1]
-        error += (y - (m * x + b)) ** 2 
-    return (error / float(len(points))) ** 0.5
-
-def display_error(m,b,error):
-    print "Slope: " + str(m)
-    print "Y Intercept: " + str(b)
-    print "Error: " + str(error)
 
 def plot_results(points,m,b):
     x_train = [point[0] for point in points]
     y_train = [point[1] for point in points]
     y_predict = [m*x + b for x in x_train]
-    plt.plot(x_train,y_predict,color='red')
-    plt.scatter(x_train,y_train,color='green')
+    plt.plot(x_train,y_predict,color='r')
+    plt.scatter(x_train,y_train,color='g')
+    plt.title("Iterative Linear Regression")
     plt.show()
 
 def init():
-    points = genfromtxt("input.csv", delimiter=",")
-    learning_rate = 0.0001
+
+    #read file into list of x,y coordinates, one set per line
+    reader = list(csv.reader(open("input.csv", "rb"), delimiter=','))
+    points = numpy.array(reader).astype('float')
+
+    #set the initial parameters
+    learning_rate = 0.0001  #step size
     b = 0 # initial y-intercept guess
     m = 0 # initial slope guess
-    num_iter = 1000
+    num_iter = 1000 #number of iterations of gradient across all points
     return points, learning_rate, b, m, num_iter
 
 def main():
     points, learning_rate, b, m, num_iter = init()
-    [b, m] = gradient_descent_runner(points, b, m, learning_rate, num_iter)
-    error = compute_mean_error(points,b,m)
-    display_error(m,b,error)
+    [b, m] = gradient_descent(points, b, m, learning_rate, num_iter)
     plot_results(points,m,b)
 
 main()
